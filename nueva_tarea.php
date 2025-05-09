b5669717372fc5976dbd2108a3861a43069a2050
Juan Arcila <juanvillamarin2000@gmail.com>
2025-05-09 16:20:22 -0500

    2

---
+++ nueva_tarea.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Crear tarea</h1>
+<br>
+<hr>
+<form method="POST" action="nueva_tarea1.php">
+    <input type="text" name="titulo" placeholder="Título" required>
+    <textarea name="descripcion" placeholder="Descripción"></textarea>
+    <button type="submit">Crear Tarea</button>
+</form>
+<hr>
+<br>
+<a href="tareas.php">Ver tareas</a>
+<br>
+<a href="logout.php">Cerrar sesion</a>
+</body>
+</html>
